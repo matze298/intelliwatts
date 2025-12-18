7e5412567b095f92aca10623b9afb0e876345657
Matthias Rath <matze29894@gmail.com>
2025-12-18 15:15:46 +0100

    Add ty to dev-reqs

--- main.py
+++ main.py
@@ -3,4 +3,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
